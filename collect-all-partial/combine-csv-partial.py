998ba13ef8e3190bc0a273dc9044b7c912f3a369
Agustina Dinamarca <agustinadinamarca@gmail.com>
2022-06-17 20:57:55 -0300

    Update combine-csv-partial.py

--- collect-all-partial/combine-csv-partial.py
+++ collect-all-partial/combine-csv-partial.py
@@ -4,7 +4,7 @@ import pandas as pd
 all_filenames_r = ["partial-res-srv-103.csv", "partial-res-srv-104.csv", "partial-res-srv-106.csv"]
 
 print('Obtaining partial-results.csv file...')
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames_r if os.path.exists(f)])
-combined_csv = combined_csv.sort_values('id', ascending=True)
+combined_csv = pd.concat([pd.read_csv(f, sep=',') for f in all_filenames if os.path.exists(f)])
+combined_csv = combined_csv.sort_values(by='id', ascending=True)
 combined_csv.to_csv("partial-results.csv", index=False, encoding='utf-8-sig')
 print('Done.')
